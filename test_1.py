9a3f79286b447e4511192a93b94097857246afc8
mohammed T <m.t.talib@hotmail.com>
2022-03-22 13:47:07 -0700

    finished laymen terms

---
+++ test_1.py
@@ -0,0 +1,16 @@
+
+"""
+The program calculates the better payment option
+first option is 100 dollarers , second is 1
+a day with it being doubled ea day for 10 days, calcls pay rate,
+function1 calcs first option functio2 2 
+
+function1 will output 100*10 days
+function2 will loop 10 times, withe a time x2 (double amount) and add to total
+
+if amount is equal, we outpiut to the user "option 1 and option 2 pays the same"
+if option1 is better, we output to the user "Option1 is better"
+if option2 is better, we output to the user "Option2 is better"
+
+
+"""
