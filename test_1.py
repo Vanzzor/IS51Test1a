ae773a9c8deeabbbba21feba7499915dc72d93fb
mohammed T <m.t.talib@hotmail.com>
2022-03-22 14:07:47 -0700

    did pseduo code

--- test_1.py
+++ test_1.py
@@ -14,3 +14,31 @@ if option2 is better, we output to the user "Option2 is better"
 
 
 """
+
+"""
+ #option1
+    return 100 * 10
+
+#option2
+ amount = 1
+ list1= []
+ loop 10 times
+    add amount list1
+    amount *=2
+    sum = sum of all items in loop
+ return sum
+
+#main
+ var1=option1
+ var2=option1
+
+ if var1=var2
+ "option1 and option 2 pays the same"
+if var1 < var2
+    option 2 is better
+    else
+    option 1 is better
+
+
+ 
+"""
